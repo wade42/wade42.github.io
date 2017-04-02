b2bfe34e69ef68209457c15056c27ea9a7ed4273
wade42 <ninetofive90@gmail.com>
2017-04-02 16:16:42 +0900

    marked 라이브러리 import  path 수정

--- app/marked/marked.js
+++ app/marked/marked.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var marked = require('../../node_modules/marked/lib/marked.js');
+var marked = require('../../core/marked/lib/marked.js');
 var Marked = (function () {
     function Marked() {
         this.marked = marked;
